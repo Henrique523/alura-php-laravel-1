6b98fa3418869390b8881ba9d60bd0f667f43e7e
Henrique <suporte@pcmsolucoes.com.br>
2020-06-01 15:15:46 -0300

    Fim parte 2.

--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('cabecalho')
-Séries
+    Séries
 @endsection
 
 @section('conteudo')
@@ -9,19 +9,36 @@ Séries
     @if(!empty($mensagem))
         <div class="alert alert-success">{{ $mensagem }}</div>
     @endif
-<a href="{{route('form_criar_serie')}}" class="btn btn-dark mb-2">Adicionar</a>
+    <a href="{{route('form_criar_serie')}}" class="btn btn-dark mb-2">Adicionar</a>
     <ul class="list-group">
         @foreach($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">{{$serie->nome}}
+                <span class="d-flex">
+                <a href="/series/{{$serie->id}}/temporadas" class="btn btn-info btn-sm mr-lg-1"><svg class="bi bi-folder-symlink" width="1em" height="1em"
+                                                             viewBox="0 0 16 16" fill="currentColor"
+                                                             xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.828 4a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 6.173 2H2.5a1 1 0 0 0-1 .981L1.546 4h-1L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3v1z"/>
+                        <path fill-rule="evenodd"
+                              d="M13.81 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zM2.19 3A2 2 0 0 0 .198 5.181l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3H2.19z"/>
+                        <path
+                            d="M8.616 10.24l3.182-1.969a.443.443 0 0 0 0-.742l-3.182-1.97c-.27-.166-.616.036-.616.372V6.7c-.857 0-3.429 0-4 4.8 1.429-2.7 4-2.4 4-2.4v.769c0 .336.346.538.616.371z"/>
+                    </svg>
+                    </i></a>
                 <form method="post" action="/series/{{$serie->id}}"
                       onsubmit="return confirm('Tem certeza?')">
                     @csrf
                     @method('DELETE')
-                    <button class="btn btn-danger btn-sm2"><svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    <button class="btn btn-danger btn-sm2"><svg class="bi bi-trash" width="1em" height="1em"
+                                                                viewBox="0 0 16 16" fill="currentColor"
+                                                                xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                            <path fill-rule="evenodd"
+                                  d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                         </svg></button>
                 </form>
+                    </span>
             </li>
         @endforeach
     </ul>
