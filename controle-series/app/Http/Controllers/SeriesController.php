70f5d4732444fa44dd1e41a767d2607989fcbc4e
Henrique <suporte@pcmsolucoes.com.br>
2020-06-01 09:57:50 -0300

    Fim aula 4.

---
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request) {
+        $series = Serie::all();
+
+        return view('series.index', compact('series'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $serie = Serie::create($request->all());
+
+        return redirect('/series');
+    }
+}
